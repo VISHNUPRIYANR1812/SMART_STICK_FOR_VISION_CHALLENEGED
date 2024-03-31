177c87934261ac2b48009c533afecf496c6650db
VISHNUPRIYAN R <134359531+VISHNUPRIYANR1812@users.noreply.github.com>
2024-03-31 08:49:19 +0530

    GPS_TRACK_MOD.ino
    Added up water , obstacle ,SOS,telegram BoT messages

--- GPS_TRACK_MOD.ino
+++ GPS_TRACK_MOD.ino
@@ -11,23 +11,59 @@ char pass[] = "Vishnu@18122002"; // Replace passkey
 #define BLYNK_PRINT Serial
 #include <WiFiClient.h>
 #include <BlynkSimpleEsp32.h>
+#include <WiFiClientSecure.h>
 #include <TinyGPS++.h> 
+#include <UniversalTelegramBot.h>
+#define BOTtoken "6738608250:AAGVkVdpl7Kf1rYVQ7qKAuQqXBJNZNVlvvI"
+#define CHAT_ID "1436137789"
+WiFiClientSecure client;
+UniversalTelegramBot bot(BOTtoken, client);
+const int buzzerPin = 5; 
+const int moisturePin = 18;
+const int trigPin = 13; // GPIO 12
+const int echoPin = 12; // GPIO 13
+bool systemInitialized = false;
 #define RXD2 16
 #define TXD2 17
 HardwareSerial neogps(2);
 TinyGPSPlus gps;
 BlynkTimer timer;
 #define INTERVAL 1000L
-void sendGps()
+bool sosAlertSent = false; // Global variable to track SOS alert status
+
+void sendSOSAlertToTelegram();
+
+void setup() 
 {
-  while(neogps.available())
+  pinMode(moisturePin, INPUT);
+  pinMode(trigPin, OUTPUT);
+  pinMode(echoPin, INPUT);
+  pinMode(buzzerPin, OUTPUT);
+  Serial.begin(115200);
+  client.setCACert(TELEGRAM_CERTIFICATE_ROOT);
+  setupWiFi();
+  Blynk.run();
+  if (!systemInitialized)
   {
-    if (gps.encode(neogps.read()))
+    sendInitMessageToTelegram();
+    systemInitialized = true;
+  }
+  neogps.begin(9600, SERIAL_8N1, RXD2, TXD2);
+  Serial.println("neogps serial initialize");
+  delay(10);
+  timer.setInterval(INTERVAL, sendGps);
+}
+
+void sendGps() 
+{
+  while(neogps.available()) 
+  {
+    if (gps.encode(neogps.read())) 
     {
       break;
     }
   }
-  if (!gps.location.isValid())
+  if (!gps.location.isValid()) 
   {
     Serial.println("Failed to read from GPS Module!");
     return;
@@ -39,25 +75,96 @@ void sendGps()
   Serial.println(latitude, 6);
   Serial.print("Longitude: ");
   Serial.println(longitude, 6);
-  Serial.print("Speed: ");
-  Serial.println(speed, 6);
-  Blynk.virtualWrite(V1, String(latitude, 6));
-  Blynk.virtualWrite(V2, String(longitude, 6));
-  Blynk.virtualWrite(V0, String(speed));
+  String latLongString = "LAT : " + String(latitude, 6) + " : LONG : " + String(longitude, 6);
+  Blynk.virtualWrite(V1, latLongString); 
   String mapLink = "http://maps.google.com/maps?q=" + String(latitude, 6) + "," + String(longitude, 6);
   Blynk.virtualWrite(V3, mapLink); 
 }
-void setup()
+
+BLYNK_WRITE(V0) 
 {
-  Serial.begin(115200);
-  Blynk.begin(auth, ssid, pass);
-  neogps.begin(9600, SERIAL_8N1, RXD2, TXD2);
-  Serial.println("neogps serial initialize");
-  delay(10);
-  timer.setInterval(INTERVAL, sendGps);
+  int value = param.asInt(); 
+  if (value == HIGH && !sosAlertSent) 
+  {
+    digitalWrite(buzzerPin, HIGH); // Activate buzzer for SOS alert
+    sendSOSAlertToTelegram();
+    sosAlertSent = true; // Mark SOS alert as sent
+  } 
+  else 
+  {
+    digitalWrite(buzzerPin, LOW);
+    sosAlertSent = false; // Reset SOS alert status for next cycle
+  }
 }
-void loop()
+
+void sendSOSAlertToTelegram()
+{
+  String message = "Alerting person \nSoS Buzzer is Set ON";
+
+  if (client.connect("api.telegram.org", 443))
+  {
+    bot.sendMessage(CHAT_ID, message, "Markdown");
+    delay(10);
+  }
+}
+
+void beepBuzzer(int beeps) // beep counter 
+{
+  for (int i = 0; i < beeps; i++) 
+  {
+    digitalWrite(buzzerPin, HIGH);
+    delay(100);
+    digitalWrite(buzzerPin, LOW);
+    delay(100);
+  }
+}
+
+void checkDistance()
+{
+  long duration, distance;
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(2);
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+  duration = pulseIn(echoPin, HIGH);
+  distance = duration * 0.034 / 2; 
+  if (distance < 40)  // set distance here
+  { 
+    beepBuzzer(2); // two beeps for 40cm distance range
+    delay(2000);
+  }
+}
+
+void checkMoisture()
+{
+  int moistureValue = digitalRead(moisturePin);
+  if (moistureValue == LOW) 
+  {
+    beepBuzzer(3); 
+    delay(1000); 
+  }
+}
+
+void sendInitMessageToTelegram()
+{
+  String message = "System initialized and ready.";
+  if (client.connect("api.telegram.org", 443))
+  {
+    bot.sendMessage(CHAT_ID, message, "Markdown");
+    delay(10);
+  }
+}
+
+void setupWiFi()
+{
+  Blynk.begin(auth, ssid, pass);
+}
+
+void loop() 
 {
   Blynk.run();
   timer.run();
+  checkDistance();
+  checkMoisture();
 }
